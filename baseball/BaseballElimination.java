119347ecabe75a7265a5d819bf9bbba8eb614e5a
William Schwartz <wkschwartz@gmail.com>
2013-04-15 18:50:57 -0400

    Implement constructor

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -7,8 +7,38 @@
  * @author William Schwartz
  */
 public class BaseballElimination {
-	// create a baseball division from given filename in format specified below
-	public BaseballElimination(String filename);
+	private final HashMap<String, Integer> teams; // Map team name to index
+	private final int[] w, l, r; // Wins, losses, remaining games
+	private final int[][] g; // Games left between teams i and j
+
+	/**
+	 * Create a baseball division from given filename in format specified below.
+	 * <p>
+	 * The input format is the number of teams in the division N followed by one
+	 * line for each team. Each line contains the team name (with no internal
+	 * whitespace characters), the number of wins, the number of losses, the
+	 * number of remaining games, and the number of remaining games against each
+	 * team in the divsion.
+	 *
+	 * @param filename The name of the file to read in as input.
+	 */
+	public BaseballElimination(String filename) {
+		In file = new In(filename);
+		int n = file.readInt();
+		teams = new HashMap<String, Integer>(n + 1, 1.0); // Will never rehash
+		w = new int[n];
+		l = new int[n];
+		r = new int[n];
+		g = new int[n][n]; // XXX may be able to cut this in half (upper triangle)
+		for (int i = 0; i < n; i++) {
+			teams.put(file.readString(), i);
+			w[i] = file.readInt();
+			l[i] = file.readInt();
+			r[i] = file.readInt();
+			for (int j = 0; j < n; j++)
+				g[i][j] = file.readInt();
+		}
+	}
 
 	// number of teams
 	public int numberOfTeams();
