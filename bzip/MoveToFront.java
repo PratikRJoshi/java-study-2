4ab0f61aea04e610a1cbe208ab85343091a873b9
William Schwartz <wkschwartz@gmail.com>
2013-04-24 13:55:50 -0400

    Implement an array-based move-to-front algorithm

--- bzip/MoveToFront.java
+++ bzip/MoveToFront.java
@@ -1,11 +1,44 @@
 public class MoveToFront {
+	private static final int R = 256;
+
 	// apply move-to-front encoding, reading from standard input and writing to
 	// standard output
-	public static void encode();
+	public static void encode() {
+		char[] chars = radixList();
+		char count, ch;
+		while (!BinaryStdIn.isEmpty()) {
+			ch = BinaryStdIn.readChar();
+			for (count = 0; ch != chars[count]; count++);
+			BinaryStdOut.write(count);
+			for (; count > 1; count--)
+				chars[count] = chars[count - 1];
+			chars[count] = ch;
+		}
+		BinaryStdOut.close();
+	}
 
 	// apply move-to-front decoding, reading from standard input and writing to
 	// standard output
-	public static void decode();
+	public static void decode() {
+		char[] chars = radixList();
+		char count, ch;
+		while (!BinaryStdIn.isEmpty()) {
+			count = BinaryStdIn.readChar();
+			ch = chars[count];
+			BinaryStdOut.write(ch);
+			for (; count > 1; count--)
+				chars[count] = chars[count - 1];
+			chars[count] = ch;
+		}
+		BinaryStdOut.close();
+	}
+
+	// Return an array list of elements of language with radix R in order.
+	private static char[] radixList() {
+		char[] rl = new char[R];
+		for (char i = 0; i < R; rl[i] = i++);
+		return rl;
+	}
 
 	// if args[0] is '-', apply move-to-front encoding
 	// if args[0] is '+', apply move-to-front decoding
