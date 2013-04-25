a926eda2dc280064ed7ade3b3553c766ea2d28ba
William Schwartz <wkschwartz@gmail.com>
2013-04-25 00:53:11 -0400

    Factor out filename generation from testing methods

--- bzip/test_stream_coders.py
+++ bzip/test_stream_coders.py
@@ -10,6 +10,8 @@ import os
 
 TEST_DATA  = os.path.join('instructor', 'test')
 MAXDIFFLENGTH = 800
+EXTS = ['.huf', '.bwt', '.mtf']
+
 if os.name == 'posix':
 	JAVA = ['java']
 	JAVAC = ['javac']
@@ -54,15 +56,19 @@ class StreamTest:
 
 	def test_encoding(self):
 		"Test that prog can encode the same as reference data"
-		encs = glob.glob(os.path.join(TEST_DATA, '*') + self.ext)
-		raws = [os.path.splitext(i)[0] for i in encs]
-		self._test_halftrip(raws, encs, '-', 'encoding')
+		self._test_halftrip(self._raws(), self._encs(), '-', 'encoding')
 
 	def test_decoding(self):
 		"Test that prog can decode reference encoded data"
-		encs = glob.glob(os.path.join(TEST_DATA, '*') + self.ext)
-		raws = [os.path.splitext(i)[0] for i in encs]
-		self._test_halftrip(encs, raws, '+', 'decoding')
+		self._test_halftrip(self._encs(), self._raws(), '+', 'decoding')
+
+	@classmethod
+	def _encs(cls):
+		return glob.glob(os.path.join(TEST_DATA, '*') + cls.ext)
+
+	@classmethod
+	def _raws(cls):
+		return [os.path.splitext(i)[0] for i in cls._encs()]
 
 	def _test_halftrip(self, from_files, to_files, cmd, action):
 		"Test that prog can do one kind of coding"
@@ -86,7 +92,7 @@ class StreamTest:
 		def cmd(f):
 			return JAVA + [self.prog, '-', '<', f, '|'] + JAVA + [self.prog, '+']
 		filename = None
-		for filename in filter(os.path.isfile, os.listdir(TEST_DATA)):
+		for filename in self._all_raws():
 			mine = subprocess.check_output(cmd(filename), shell=True)
 			with open(filename, 'rb') as f:
 				theirs = f.read()
@@ -97,6 +103,15 @@ class StreamTest:
 				self.fail('roundtrip: %r' % filename)
 		if filename is None:
 			raise FileNotFoundError("No files tested")
+
+	@staticmethod
+	def _all_raws():
+		"Return an iterable of all the unencoded/raw testing files."
+		fnames = os.listdir(TEST_DATA)
+		fnames = map(lambda s: os.path.join(TEST_DATA, s), fnames)
+		fnames = filter(os.path.isfile, fnames)
+		fnames = filter(lambda s: os.path.splitext(s)[1] not in EXTS, fnames)
+		return fnames
 
 
 class TestMoveToFront(StreamTest, unittest.TestCase):
